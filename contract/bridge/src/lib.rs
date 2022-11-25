3ebee89614de979d7b9de078c05154bcee9b5e54
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-25 18:10:50 +0400

    changed storage structure

--- contract/bridge/src/lib.rs
+++ contract/bridge/src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::env::predecessor_account_id;
 use near_sdk::{self, collections::UnorderedSet, AccountId};
-use near_sdk::{near_bindgen, BorshStorageKey, PanicOnDefault};
+use near_sdk::{near_bindgen, BorshStorageKey, PanicOnDefault, Promise};
 
 #[derive(BorshSerialize, BorshDeserialize, BorshStorageKey)]
 pub enum StorageKey {
@@ -12,6 +12,7 @@ pub enum StorageKey {
 #[derive(Debug, BorshSerialize, BorshDeserialize, PanicOnDefault)]
 pub struct Bridge {
     token_address: AccountId,
+    lite_client_address: AccountId,
     admin_list: UnorderedSet<AccountId>,
 }
 
@@ -19,11 +20,16 @@ pub struct Bridge {
 impl Bridge {
     #[init]
     #[private]
-    pub fn init(token_address: AccountId) -> Self {
+    pub fn init(token_address: AccountId, lite_client_address: AccountId) -> Self {
         Self {
             token_address,
+            lite_client_address,
             admin_list: UnorderedSet::new(StorageKey::AdminList),
         }
+    }
+
+    pub fn validate_transfer(&self, block_number: u64, account_address: String, amount: u128) {
+        // let logs_filter = Promise::new(self.lite_client_address.clone());
     }
 
     pub fn check_admin(&self) -> bool {
--- contract/lite_client/src/lib.rs
+++ contract/lite_client/src/lib.rs
@@ -1,28 +1,31 @@
 use near_sdk::{
     self,
     borsh::{self, BorshDeserialize, BorshSerialize},
+    collections::UnorderedSet,
     require,
     store::UnorderedMap,
     PanicOnDefault,
 };
 use near_sdk::{near_bindgen, BorshStorageKey};
 
-pub type BlockNumber = u64;
+pub type TransactionId = [u8; 32];
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
-    BlockHeaders,
+    EventLogs,
+    Topics,
 }
 
 #[derive(Debug, BorshSerialize, BorshDeserialize)]
-pub struct BlockHeader {
-    logs_filter: Vec<u8>,
+pub struct EventLogs {
+    topics: UnorderedSet<Vec<u8>>,
+    block_hash: Vec<u8>,
 }
 
 #[near_bindgen]
 #[derive(Debug, BorshSerialize, BorshDeserialize, PanicOnDefault)]
 pub struct LiteClient {
-    block_headhers: UnorderedMap<BlockNumber, BlockHeader>,
+    event_logs: UnorderedMap<TransactionId, EventLogs>,
 }
 
 #[near_bindgen]
@@ -31,29 +34,50 @@ impl LiteClient {
     #[private]
     pub fn init() -> Self {
         Self {
-            block_headhers: UnorderedMap::new(StorageKey::BlockHeaders),
+            event_logs: UnorderedMap::new(StorageKey::EventLogs),
         }
     }
 
-    pub fn get_logs_filter(&self, block_number: BlockNumber) -> Vec<u8> {
-        match self.block_headhers.get(&block_number) {
-            Some(header) => header.logs_filter.clone(),
-            _ => panic!("Logs filter doesn't exist"),
+    pub fn get_event_logs(&self, transaction_id: [u8; 32]) -> &EventLogs {
+        let transaction_id = transaction_id as TransactionId;
+        match self.event_logs.get(&transaction_id) {
+            Some(logs) => return logs,
+            _ => {
+                panic!("Logs doesn't exist")
+            }
         }
     }
 
-    pub fn add_block_header(&mut self, block_number: BlockNumber, logs_filter: Vec<u8>) {
-        require!(if_caller_is_admin(), "Caller isn't owner");
-        self.block_headhers
-            .insert(block_number, BlockHeader { logs_filter });
-    }
+    pub fn add_event_logs(
+        &mut self,
+        transaction_id: [u8; 32],
+        topics: Vec<Vec<u8>>,
+        block_hash: Vec<u8>,
+    ) {
+        let mut logs = EventLogs {
+            topics: UnorderedSet::new(StorageKey::Topics),
+            block_hash,
+        };
 
-    pub fn remove_block_header(&mut self, block_number: BlockNumber) {
-        require!(if_caller_is_admin(), "Caller isn't owner");
-        self.block_headhers.remove(&block_number);
+        for e in topics.iter() {
+            logs.topics.insert(e);
+        }
+
+        self.event_logs.insert(transaction_id, logs);
     }
 }
 
 fn if_caller_is_admin() -> bool {
     return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+
+    fn get_context() -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+    }
 }
