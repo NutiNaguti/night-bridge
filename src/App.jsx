fcf7ac64a8fce641105674f8c556a639d39e26c8
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-29 01:35:15 +0400

    dev in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { ethers } from "ethers";
 import { getConfig } from "./config";
 import "./App.css";
 
+const ethereum = window.ethereum;
+
+const abi = [
+  "function lock(uint256) external returns (bytes32)"
+]
+
 const {
   format: { formatNearAmount },
 } = utils;
-
-const accountChange = () => { };
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -24,7 +28,6 @@ const App = () => {
   const addressRef = useRef(null);
 
   useEffect(() => {
-    const { ethereum } = window;
     const checkMetamaskAvailability = async () => {
       if (!ethereum) {
         setHaveMetamask(false);
@@ -78,28 +81,48 @@ const App = () => {
     setMetamaskConnected(true);
   }
 
-  function handleSend() {
+  const lockTokens = async () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const walletAddress = accountEth;
+    const signer = provider.getSigner(walletAddress);
+    const BridgeContract = new ethers.Contract("0x9431f9bba577B037D97ad6F7086a00eFB572c871", abi, signer);
+    const tx = await BridgeContract.lock(ethers.BigNumber.from(amountRef.current.value));
+    const receipt = await tx.wait();
+    console.log("topic: ", receipt.logs[2].topics[1]);
+    console.log("blockNumber: ", receipt.logs[2].blockNumber);
+    console.log("txHash: ", receipt.logs[2].transactionHash);
+
+    await completeTransfer(receipt.logs[2].blockNumber, receipt.logs[2].topics[1], addressRef.current.value);
+  }
+
+  const completeTransfer = async (blockNumber, topic, address) => {
+    //TODO implement near transfer
+  }
+
+  const handleSend = async () => {
     console.log(amountRef.current.value);
     console.log(addressRef.current.value);
+
+    await lockTokens();
   }
 
   return (
     <div className="App">
       <header className="AppHeader">
+        <h1>Crystal Bridge</h1>
         <button className="navbar-btn" onClick={() => connectMetamask()}>Login ETH</button>
         <button className="navbar-btn" onClick={() => handleLogin()}>Login NEAR</button>
       </header>
 
       <div>
-        <input id="text" ref={amountRef} />
+        <input id="text" ref={amountRef} />&nbsp;&#8212;&nbsp;<span>Amount</span>
       </div>
       <div>
-        <input id="text" ref={addressRef} />
+        <input id="text" ref={addressRef} />&nbsp;&#8212;&nbsp;<span>NEAR Address</span>
       </div>
       <div>
       </div>
       <button className="Button" onClick={() => handleSend()}>Send</button>
-      <button className="Button">Get</button>
     </div>
   );
 };
